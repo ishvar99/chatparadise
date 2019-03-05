62c4e21721225d973c97b90f2c0e6e3ae959d11f
Ishan Varshney <ishanvarshney99@gmail.com>
2019-03-05 19:54:02 +0530

    Added socket.io and connect,disconnect event handlers

---
+++ public/index.js
@@ -0,0 +1,7 @@
+var socket=io();
+socket.on('connect',()=>{
+	console.log("connected to server!")
+})
+socket.on('disconnect',()=>{
+	console.log("disconnected from server!")
+})
--- server/app.js
+++ server/app.js
@@ -1,9 +1,19 @@
 const express=require('express'),
+      socketIO=require('socket.io'),
+      http=require('http'),
       path=require('path'),
       publicPath=path.join(__dirname,'../public'),
-       app=express();
-const port=process.env.PORT||3000;
+      port=process.env.PORT||3000;
+var   app=express(),
+      server=http.createServer(app),//behind the scenes it gets called once you call app.listen()
+      io=socketIO(server);
+      io.on('connection',(socket)=>{
+      	console.log("new user connected!");
+      	socket.on('disconnect',()=>{
+      		console.log("user disconnected!")
+      	})
+      })
 app.use(express.static(publicPath));
-app.listen(port,()=>{
+server.listen(port,()=>{
 	console.log(`Server is up on ${port}!`)
 })
