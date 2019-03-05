0c7c181c6ce0ae3606d023bff79383d64d9dedfd
Ishan Varshney <ishanvarshney99@gmail.com>
2019-03-05 18:57:36 +0530

    typo resolved

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express=require('express'),
       path=require('path'),
       publicPath=path.join(__dirname,'../public'),
        app=express();
-const port=process.env.port||3000;
+const port=process.env.PORT||3000;
 app.use(express.static(publicPath));
 app.listen(port,()=>{
 	console.log(`Server is up on ${port}!`)
